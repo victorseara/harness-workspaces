cb22237fc4a17069684034e1231247f6ef82b222
victorseara <victorseara.me@gmail.com>
2025-11-10 16:26:22 -0300

    update modules

--- modules/bff/main.js
+++ modules/bff/main.js
@@ -2,6 +2,7 @@
  * A placeholder AWS Lambda function.
  * This function is used by OpenTofu as a stand-in for actual Lambda functions when deploying infrastructure.
  * The actual function code is implemented elsewhere and deployed separately. 
+ *
 */
 exports.handler = async (event) => {
   console.log('event', event);
