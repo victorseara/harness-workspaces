da2ce4972a1a8437ca406c1eceb0530a2ea7deac
victorseara <victorseara.me@gmail.com>
2025-11-10 16:36:17 -0300

    deploy iac

--- modules/bff/main.js
+++ modules/bff/main.js
@@ -2,7 +2,6 @@
  * A placeholder AWS Lambda function.
  * This function is used by OpenTofu as a stand-in for actual Lambda functions when deploying infrastructure.
  * The actual function code is implemented elsewhere and deployed separately. 
- *
 */
 exports.handler = async (event) => {
   console.log('event', event);
