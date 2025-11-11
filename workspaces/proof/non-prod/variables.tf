2b8cfb31528afeae3014059a2d9f5d596466c363
victorseara <victorseara.me@gmail.com>
2025-11-11 12:03:29 -0300

    deploy proof workspace

--- workspaces/proof/non-prod/variables.tf
+++ workspaces/proof/non-prod/variables.tf
@@ -3,6 +3,3 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
-
-# Each BFF module manages its own configuration via locals
-# No BFF-specific variables needed at the workspace level
